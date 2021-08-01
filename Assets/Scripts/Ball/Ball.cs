dcf1e1f656e0421468dd15cb8f5eca2203e06dea
me11y <imelon2013@gmail.com>
2021-08-02 01:37:22 +0300

    [*] moved platform destroying logic to passed platform segment

--- Assets/Scripts/Ball/Ball.cs
+++ Assets/Scripts/Ball/Ball.cs
@@ -2,11 +2,5 @@
 
 public class Ball : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.TryGetComponent(out TriggerSegment triggerSegment))
-        {
-            triggerSegment.GetComponentInParent<Platform>().Break();
-        }
-    }
+
 }
---
+++ Assets/Scripts/Platform/Segment/PassedPlatformSegment.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PassedPlatformSegment : PlatformSegment
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Ball ball))
+        {
+            GetComponentInParent<Platform>().Break();
+        }
+    }
+}
