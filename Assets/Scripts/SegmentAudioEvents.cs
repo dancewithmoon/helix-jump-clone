941dc927cc67643dae7ec71f47007509ea4afe69
me11y <imelon2013@gmail.com>
2021-08-18 01:40:10 +0300

    [+] added ball jump sounds

---
+++ Assets/Scripts/SegmentAudioEvents.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(PlatformSegment))]
+public class SegmentAudioEvents : MonoBehaviour
+{
+    public UnityEvent Touched;
+    public UnityEvent Triggered;
+
+    private PlatformSegment _segment;
+
+    private void Awake()
+    {
+        _segment = GetComponent<PlatformSegment>();
+    }
+
+    private void OnEnable()
+    {
+        _segment.BallTouched += InvokeTouchEvent;
+        _segment.BallTriggered += InvokeTriggeredEvent;
+    }
+
+    private void InvokeTouchEvent(Ball ball)
+    {
+        Touched?.Invoke();
+    }
+
+    private void InvokeTriggeredEvent(Ball ball)
+    {
+        Triggered?.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        _segment.BallTouched -= InvokeTouchEvent;
+        _segment.BallTriggered -= InvokeTriggeredEvent;
+    }
+}
