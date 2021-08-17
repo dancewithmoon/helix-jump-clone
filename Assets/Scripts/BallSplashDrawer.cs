32212e36e768487fc1a9cf912829fdb29b3dfea0
me11y <imelon2013@gmail.com>
2021-08-18 01:16:55 +0300

    [*] optimized FinishPlatform flow [+] added splash after ball touched platform

---
+++ Assets/Scripts/BallSplashDrawer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class BallSplashDrawer : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _splashPrefab;
+    [SerializeField] private float _yOffset = 0.13f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.TryGetComponent(out PlatformSegment platformSegment))
+        {
+            Vector3 contactPoint = collision.GetContact(0).point;
+            contactPoint.y = transform.position.y - _yOffset;
+            GameObject splash = Instantiate(_splashPrefab.gameObject, platformSegment.transform);          
+            splash.transform.position = contactPoint;
+        }
+    }
+}
--- Assets/Scripts/Game Process/Platform/FinishPlatform.cs
+++ Assets/Scripts/Game Process/Platform/FinishPlatform.cs
@@ -1,18 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class FinishPlatform : Platform
 {
-    private bool _isTouched = false;
+    private List<PlatformSegment> _segments;
 
     public event Action Touched;
 
-    private void OnCollisionEnter(Collision collision)
+
+    private void Awake()
     {
-        if (collision.gameObject.TryGetComponent(out Ball ball) && _isTouched == false)
-        {
-            _isTouched = true;
-            Touched?.Invoke();
-        }
+        _segments = GetComponentsInChildren<PlatformSegment>().ToList();
+    }
+
+    private void OnEnable()
+    {
+        _segments.ForEach((segment) => segment.BallTouched += Finish);
+    }
+
+    private void Finish(Ball ball)
+    {
+        _segments.ForEach((segment) => segment.enabled = false);
+        Touched?.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        _segments.ForEach((segment) => segment.BallTouched -= Finish);
     }
 }
