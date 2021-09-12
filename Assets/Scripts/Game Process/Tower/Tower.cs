a2d0114475d7501aa605f6b3a69c3b47e0d94365
me11y <imelon2013@gmail.com>
2021-09-12 18:45:14 +0300

    [*] encapsulated platforms list

--- Assets/Scripts/Game Process/Tower/Tower.cs
+++ Assets/Scripts/Game Process/Tower/Tower.cs
@@ -2,17 +2,19 @@
 
 public class Tower
 {
+    public List<Platform> _platforms;
+
+    public IReadOnlyList<Platform> Platforms => _platforms.AsReadOnly();
     public StartPlatform StartPlatform { get; private set; }
-    public List<Platform> Platforms { get; private set; }
     public FinishPlatform FinishPlatform { get; private set; }
     public Beam Beam { get; private set; }
 
-    public int PlatformsCount => Platforms.Count + 2;
+    public int PlatformsCount => _platforms.Count + 2;
 
     public Tower(StartPlatform startPlatform, List<Platform> platforms, FinishPlatform finishPlatform, Beam beam)
     {
         StartPlatform = startPlatform;
-        Platforms = platforms;
+        _platforms = platforms;
         FinishPlatform = finishPlatform;
         Beam = beam;
     }
